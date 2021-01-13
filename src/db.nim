6a72e9a43e60a30a42e295f75e3654dc782c49c3
Daehee <hello@daehee.com>
2021-01-13 11:12:14 -0700

    WIP

--- src/db.nim
+++ src/db.nim
@@ -20,28 +20,21 @@ proc initDbClient*(connStr: string): Future[DBClient] {.async.} =
 #proc setDbClient*(connStr: string) =
 #  dbClient = waitFor initDbClient(connStr)
 
-proc checkHealth*(cl: DbClient): Future[string] {.async.} =
-  let tmpConn = await connect(cl.connStr)
-  let ver = tmpConn.getServerVersion()
-  result = $ver
-  tmpConn.close()
-
 template parseCveRow*(row: Row): Cve =
   let
     cveId = row[1]
     cveData = parseJson(row[2])
     description = cveData["cve"]["description"]["description_data"][0]["value"].getStr()
-  Cve(cveId: cveId, description: description)
+    pubDate = parsePgDateTime(cveData["publishedDate"].getStr())
+  Cve(cveId: cveId, description: description, pubDate: pubDate)
 
 proc getCveBySequence*(cl: DbClient; year, seq: int): Future[Cve] {.async.} =
-  # TODO move this to models?
   var params = {"year": year, "seq": seq}.toTable
   let res = await cl.conn.exec("select id, cve_id, data from cves where year = $1 and sequence = $2", params["year"], params["seq"])
   let row = res[0].getRow()
   result = parseCveRow(row)
 
 proc getCveByCveId*(cl: DbClient, cveId: string): Future[Cve] {.async.} =
-  # TODO move this to models?
   var param = cveId
   let res = await cl.conn.exec("select id, cve_id, data from cves where cve_id = $1", param)
   let row = res[0].getRow()
@@ -50,7 +43,3 @@ proc getCveByCveId*(cl: DbClient, cveId: string): Future[Cve] {.async.} =
 proc close*(cl: DbClient) =
   cl.conn.close()
 
-#proc connect
-# Establish connection to PostgreSQL server
-
-#proc get
--- src/models/cve.nim
+++ src/models/cve.nim
@@ -1,3 +1,4 @@
+import std/times
 
 type
   Cve* = object
@@ -5,4 +6,19 @@ type
     year*: int
     sequence*: int
     description*: string
-    pubDate*: string
+    pubDate*: DateTime
+    refUrls*: seq[string]
+    cvss3*: float
+
+  Product* = object
+    name*: string
+    uriShort*: string
+
+  Vendor* = object
+    name*: string
+    products*: seq[Product]
+
+proc parsePgDateTime*(s: string): DateTime =
+  # Example: "2006-01-02T15:04Z"
+  let layout = "yyyy-MM-dd'T'HH:mm'Z'"
+  s.parse(layout, utc())
