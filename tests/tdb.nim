069469b0c3d8cffc17100b33e71e6dfe79c712aa
Daehee <hello@daehee.com>
2021-01-20 10:48:26 -0700

    add cvss check to testdb

--- tests/tdb.nim
+++ tests/tdb.nim
@@ -11,6 +11,8 @@ template checkCve(cve: Cve) =
   check cve.description == """Vulnerability in the Oracle WebLogic Server product of Oracle Fusion Middleware (component: Console). Supported versions that are affected are 10.3.6.0.0, 12.1.3.0.0, 12.2.1.3.0, 12.2.1.4.0 and 14.1.1.0.0. Easily exploitable vulnerability allows unauthenticated attacker with network access via HTTP to compromise Oracle WebLogic Server. Successful attacks of this vulnerability can result in takeover of Oracle WebLogic Server. CVSS 3.1 Base Score 9.8 (Confidentiality, Integrity and Availability impacts). CVSS Vector: (CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H)."""
   check cve.pubDate.month().ord() == 10
   check cve.refUrls.len() == 3
+  check cve.cvss3.get().score == "9.8"
+  check cve.cvss3.get().severity == "CRITICAL"
 
 suite "db tests":
   let connStr = "postgres://postgres:yeetya123@localhost:5432/cvebase_development"
