4fa0fe54c0d4106cece379daee5004c54c2e6221
Daehee <hello@daehee.com>
2021-01-27 15:19:40 -0700

    add title and meta to welcome index

--- src/controllers/welcome_ctrl.nim
+++ src/controllers/welcome_ctrl.nim
@@ -18,4 +18,7 @@ proc showWelcome*(ctx: Context) {.async.} =
   let cves = await db.getWelcomeCves()
   let hacktivities = await db.getWelcomeHacktivities()
 
+  ctx.ctxData["title"] = "cvebase: Discover CVE vulnerabilities & PoC exploits"
+  ctx.ctxData["description"] = "Share, discuss, and reverse the latest security vulnerabilities and PoC exploits. Join the thousands of security researchers, pentesters, and bug bounty hunters who use cvebase as their vulnerability knowledge base."
+
   resp ctx.renderMain(ctx.renderWelcome(researchers, cves, hacktivities))
